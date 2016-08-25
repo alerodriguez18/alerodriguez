6d48d73339b71043639d48b3dd6368f32dbbef33
Alejandro Rodríguez <alerr18@gmail.com>
2016-08-24 22:37:46 -0500

    Uploading proyect

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Mi portafolio</title>
+	<link rel="stylesheet" href="static/css/bootstrap.min.css">
+	<link rel="stylesheet" href="static/css/bootstrap.min.css.map">
+</head>
+<body>
+	
+</body>
+</html>
