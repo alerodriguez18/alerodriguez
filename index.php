f75e2ed030cfda0a2e5e09deea8447af3c435c8d
Alejandro Rodríguez <alerr18@gmail.com>
2016-09-27 01:06:03 -0500

    50% of the structure realized.

--- index.php
+++ index.php
@@ -5,8 +5,92 @@
 	<title>Mi portafolio</title>
 	<link rel="stylesheet" href="static/css/bootstrap.min.css">
 	<link rel="stylesheet" href="static/css/bootstrap.min.css.map">
+	<link rel="stylesheet" href="static/css/main.css">
 </head>
-<body>
-	
+<body data-spy="scroll" data-target="#navbar-principal">
+	<div id="navbar-principal">
+		<nav class="navbar navbar-inverse navbar-fixed-top"> 
+			<div class="container-fluid"> 
+				<div class="navbar-header"> 
+					<button type="button" class="collapsed navbar-toggle" data-toggle="collapse" data-target="#aboutMe-menu" aria-expanded="false"> 
+						<span class="sr-only">Toggle navigation</span> 
+						<span class="icon-bar"></span> 
+						<span class="icon-bar"></span> 
+						<span class="icon-bar"></span> 
+					</button> 
+				</div> 
+				<div class="collapse navbar-collapse" id="aboutMe-menu"> 
+					<ul class="nav navbar-nav"> 
+						<li class="active">
+							<a href="#personalDetails">Personal details</a>
+						</li>
+						<li>
+							<a href="#summary">Summary</a>
+						</li> 
+						<li>
+							<a href="#workExperience">Work Experience</a>
+						</li> 
+						<li>
+							<a href="#education">Education</a>
+						</li>
+						<li>
+							<a href="#languageSkills">Language & Personal skills</a>
+						</li>
+						<li>
+							<a href="#otherInterested">Other information & Interest</a>
+						</li>  
+					</ul> 
+				</div> 
+			</div> 
+		</nav>
+	</div>
+	<div id="personalDetails">
+		<figure class="aboutMe-picture">
+			<img src="static/img/Me.jpg" alt="Alejandro Rodríguez">
+			<figcaption>
+				Hi, I'm Alejandro Rodríguez<br>Web developer (Front-End)
+			</figcaption>
+		</figure>
+		<div class="aboutme-info">
+			<span>Name: Gustavo Alejandro Rodríguez Rodríguez</span>
+			<span>Bithdate: June 18, 1992</span>
+			<span>Cell Phone: (+52)5591688812</span>
+			<span>Email: alerr18@gmail.com</span>
+		</div>
+	</div>
+	<div id="summary">
+		<h1>Summary</h1>
+		<ul>
+			<li>HTML5</li>
+			<li>CSS3</li>
+			<li>JavaScript</li>
+			<li>Bootstrap framework</li>
+			<li>Materialize framework</li>
+			<li>Stylus</li>
+			<li>Jquery</li>
+			<li>PHP</li>
+			<li>Version control system with Git.</li>
+			<li>Linux console</li>
+			<li>MySql data base</li>
+			<li>Java</li>
+			<li>C++</li>
+		</ul>
+	</div>
+	<div id="workExperience">
+		<h1>Work Experience</h1>
+		<div>
+			<span>February 2014 - July 2014</span>
+		</div>
+	</div>
+	<div id="education">
+	</div>
+	<div id="languageSkills">
+	</div>
+	<div id="otherInterested">
+	</div>
 </body>
+<footer>
+	<script src="static/js/jquery-3.1.1.min.js"></script>
+	<script src="static/js/bootstrap.min.js"></script>
+</footer>
 </html>
